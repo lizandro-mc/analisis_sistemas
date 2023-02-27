0f0fae528519a46de43678b1b634ec11deaa1b1f
Lizandro Ramirez <117939390+lizandro-mc@users.noreply.github.com>
2023-02-27 15:37:13 -0400

    Intial commmit

---
+++ api_frutsworld/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  msqlUser: process.env.MSQLUSER,
+  msqlHost: process.env.MSQLHOST,
+  msqlDatabase: process.env.MSQLDATABASE,
+  msqlPassword: process.env.MSQLPASSWORD,
+  msqlPort: process.env.MSQLPORT,
+};
