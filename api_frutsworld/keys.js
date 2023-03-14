c1269588a3300a52f987f9eaa51e8f10d636ba54
lizandro-mc <lizandro@mctekk.com>
2023-03-14 17:06:47 -0400

    feat: add example multicontainer app

--- api_frutsworld/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  msqlUser: process.env.MSQLUSER,
-  msqlHost: process.env.MSQLHOST,
-  msqlDatabase: process.env.MSQLDATABASE,
-  msqlPassword: process.env.MSQLPASSWORD,
-  msqlPort: process.env.MSQLPORT,
-};
